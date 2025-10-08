4ff19a19c265093496655f05d154b521507e5cbc
faisale <fa6769@gmaile.com>
2025-10-08 17:48:39 +0600

    running work

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import AppCard from "../components/AppCard";
 import appsData from "../data/app"; // JSON data
+import { FaApple, FaGooglePlay } from "react-icons/fa";
 
 export default function Home() {
     const navigate = useNavigate();
@@ -10,13 +11,50 @@ export default function Home() {
     return (
         <div className="max-w-7xl mx-auto px-6 py-8">
             {/* Banner */}
-            <section className="text-center py-12 bg-indigo-100 rounded-lg mb-12">
-                <h1 className="text-4xl font-bold mb-4">Discover Amazing Apps</h1>
-                <p className="text-gray-700 mb-6">Install apps easily and manage them effortlessly.</p>
+            <section className="text-center py-12   mb-12 ">
+                <h1 className="text-7xl font-bold mb-4">We Build</h1>
+
+                <h1 className="text-7xl font-bold mb-4">  <span className="bg-gradient-to-r from-purple-600 to-purple-600 bg-clip-text text-transparent">
+                    Productive
+                </span> Apps</h1>
+
+
+                <p className="text-gray-400 mb-8">At HERO.IO , we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
+
+          
+
                 <div className="flex justify-center gap-4">
-                    <a href="#" className="bg-indigo-600 text-white px-6 py-3 rounded-lg hover:bg-indigo-700">App Store</a>
-                    <a href="#" className="bg-green-600 text-white px-6 py-3 rounded-lg hover:bg-green-700">Play Store</a>
+                    <a
+                        href="https://play.google.com/store"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="flex items-center gap-2 bg-white text-black px-6 py-3 rounded-lg hover:bg-gray-200"
+                    >
+                        <img src="fi_16076057.png" alt="Google Play" className="w-6 h-6" />
+                        <span>Play Store</span>
+                    </a>
+
+                    <a
+                        href="https://www.apple.com/app-store/"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="flex items-center gap-2 bg-white text-black px-6 py-3 rounded-lg hover:bg-gray-200"
+                    >
+                        <img src="Group.png" alt="App Store" className="w-6 h-6" />
+                        <span>App Store</span>
+                    </a>
                 </div>
+
+
+
+
+
+
+
+
+
+
+
             </section>
 
             {/* States Section */}
